7bf5c0799d19e87a6b031cb5f5386ed30ed993aa
mojun <mojun@longmaster.com.cn>
2024-12-23 21:17:44 +0800

    第四节

---
+++ 第4节/const.php
@@ -0,0 +1,19 @@
+<?php
+
+// 1、第一种定义方式 const关键词
+
+const PI = 3.14;
+
+var_dump(PI);
+
+
+// 2、使用define函数进行定义常量
+
+
+define("MY_APP", "Blog");
+
+var_dump(MY_APP);
+
+
+
+
---
+++ 第4节/inc.php
@@ -0,0 +1,8 @@
+<?php
+
+
+// 包含math.php进来
+// include_once 只是包含一次
+include "math.php";
+
+var_dump(PI);
